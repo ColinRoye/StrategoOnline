dfd3358c30a59c802601e92d54a163f5273cd51e
brian.yi <brian.yi@stonybrook.edu>
2019-09-16 17:50:04 -0400

    added hello world message

--- src/main/java/com/cerulean/hw1/Stratego/StrategoApplication.java
+++ src/main/java/com/cerulean/hw1/Stratego/StrategoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StrategoApplication {
 
 	public static void main(String[] args) {
+	    System.out.println("a statement");
 		SpringApplication.run(StrategoApplication.class, args);
 
 	}
