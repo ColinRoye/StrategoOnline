6d0034a08137c56aab3e1903f50bc02969a70611
Khurram Malik <khurrammalik97@gmail.com>
2019-09-19 18:21:58 -0400

    Added Board, Game and Piece Model

---
+++ src/main/java/cerulean/hw1/models/Piece.java
@@ -0,0 +1,64 @@
+package cerulean.hw1.models;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import java.util.*;
+
+
+public class Piece{
+
+    public String type;
+    public boolean playerPiece;
+    public int value;
+    public boolean hidden;
+    public int dof; //degree of freedom
+
+    //COnstructor
+    public Piece(){}
+
+    public Piece(String type,int value,int dof){
+        this.type = type;
+        this.value = value;
+        this.dof = dof;
+        this.hidden = true;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isPlayerPiece() {
+        return playerPiece;
+    }
+
+    public void setPlayerPiece(boolean playerPiece) {
+        this.playerPiece = playerPiece;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public boolean isHidden() {
+        return hidden;
+    }
+
+    public void setHidden(boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    public int getDof() {
+        return dof;
+    }
+
+    public void setDof(int dof) {
+        this.dof = dof;
+    }
+}
