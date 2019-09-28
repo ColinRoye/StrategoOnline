4761ae8beff17fd8f458831ca8d570aa56d63f20
ColinRoye <croye97@gmail.com>
2019-09-27 20:09:07 -0400

    added pages back

---
+++ src/main/java/cerulean/hw1/Pages/ViewController.java
@@ -0,0 +1,22 @@
+
+package cerulean.hw1.Pages;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @RequestMapping("/register")
+    public String register() {
+        return "register";
+    }
+    @RequestMapping("/play")
+    public String play() {
+        return "play";
+    }
+
+}
